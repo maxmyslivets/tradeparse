3207e3a77d4d9fe7594711e6f055912192870870
maxi <by.mikky1206@gmail.com>
2024-10-08 02:22:10 +0300

    изменен промежуток ротации

--- logger/__init__.py
+++ logger/__init__.py
@@ -1,4 +1,4 @@
 from loguru import logger as _logger
 
 log = _logger
-log.add(f"data/errors.log", level='ERROR', rotation="5 seconds")
+log.add(f"data/errors.log", level='ERROR', rotation="1 week")
